6d0756892afde2190b640ddf49b3dee2c2d852a0
Moustique <112151515+moustique-cat@users.noreply.github.com>
2025-05-18 16:18:57 -0400

    Add input and output folder, look for the image in input folder

--- image-resizer/resize_image.py
+++ image-resizer/resize_image.py
@@ -1,16 +1,24 @@
-#script to resize images
-
-#run in venv
 from PIL import Image
+import os
 
-# Load the image
-img = Image.open("input.jpg")
+resize_factor = 0.5
 
-# Resize it
-new_height = 600
-resized_img = img.thumbnail((new_height))
+input_folder = "imageInput"
+output_folder = "imageOutput"
 
-# Save it
-resized_img.save("output.jpg")
+os.makedirs(output_folder, exist_ok=True)
 
-print("Image resized and saved as output.jpg")
+valid_extensions = (".jpg", ".jpeg", ".png", ".bmp", ".gif")
+
+for filename in os.listdir(input_folder):
+    if filename.lower().endswith(valid_extensions):
+        input_path = os.path.join(input_folder, filename)
+        output_path = os.path.join(output_folder, filename)
+
+        
+        with Image.open(input_path) as img:
+            original_height = img.height
+            new_height = (int(original_height * resize_factor))
+            resized_img = img.resize(new_height)
+            resized_img.save(output_path)
+            print(f"Resized {filename} from {original_size} to {new_size} in {output_path}.")
